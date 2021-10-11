f032c2f637d460fdcca479a7d7debb729db527bb
graysonkoy <17900870+graysonkoy@users.noreply.github.com>
2021-10-11 17:35:35 +1000

    mongo clip data uploading

--- server/src/services/mongo.ts
+++ server/src/services/mongo.ts
@@ -1,4 +1,4 @@
-import { MongoClient, Collection, Db } from "mongodb";
+import { MongoClient, Collection, Db, ObjectId } from "mongodb";
 
 class Database {
 	client: MongoClient;
@@ -8,6 +8,16 @@ class Database {
 		this.client = new MongoClient(process.env.DB_URI);
 
 		await this.client.connect();
+
+		this.db = this.client.db(process.env.DB_TABLE);
+	};
+
+	addClip = async (slug: string, data: Object) => {
+		const collection = this.db.collection("clips");
+		await collection.insertOne({
+			slug,
+			data,
+		});
 	};
 }
 
--- server/src/services/twitchApi.ts
+++ server/src/services/twitchApi.ts
@@ -1,18 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
 export async function getClipInfo(url) {
-    if (!url.includes('https://www.twitch.tv/')) throw 'invalid clip url';
+	if (!url.includes("https://www.twitch.tv/")) throw "Invalid clip url";
 
-    const slug = url.split('/').pop();
-    console.log(slug);
-    const request = await axios.get(`https://api.twitch.tv/helix/clips?id=${slug}`, {
-        headers: {
-            'Authorization': `Bearer ${process.env.TWITCH_ACCESS_TOKEN}`,
-            'Client-ID': `${process.env.TWITCH_CLIENT_ID}`
-        }
-    });
-    
-    console.log(request.data);
-    if(!request.data.slug) throw 'clip doesnt exist';
-    return request.data;
+	const slug = url.split("/").pop();
+
+	const request = await axios.get(
+		`https://api.twitch.tv/helix/clips?id=${slug}`,
+		{
+			headers: {
+				Authorization: `Bearer ${process.env.TWITCH_ACCESS_TOKEN}`,
+				"Client-ID": `${process.env.TWITCH_CLIENT_ID}`,
+			},
+		}
+	);
+
+	if (!request.data.slug) throw "Clip doesn't exist";
+
+	return request.data;
 }
